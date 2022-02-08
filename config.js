f80a0093a84c72d9034fe621c06e02dc30854d6d
Explorer <76149254+Rajshree-Sagacity@users.noreply.github.com>
2022-02-08 18:37:27 +0530

    Add files via upload
    save

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBT4EgTzemxL-e-YcMmlXAXL_4gDtb_HkQ",
+     authDomain: "logindemo-322509.firebaseapp.com",
+     databaseURL: "https://logindemo-322509-default-rtdb.firebaseio.com",
+     projectId: "logindemo-322509",
+     storageBucket: "logindemo-322509.appspot.com",
+     messagingSenderId: "722422719311",
+     appId: "1:722422719311:web:327d0c156cfb28560cdb3a"
+ };
